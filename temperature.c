b20c9341739a0cef6d5ff735df6a5cba5f615597
mb6ockatf <mb6ockatf@relay.firefox.com>
2023-05-07 14:00:05 +0300

    feat: temperature.c

---
+++ temperature.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+// Celsius = (5/9) * (Farenheit - 32)
+int main(){
+	int farenheit, celsius, lower, upper, step;
+	char farenheit_column[10] = "Farenheit";
+	char celsius_column[10] = "Celsius";
+	lower = 0;
+	upper = 300;
+	step = 20;
+	farenheit = lower;
+	printf("%12s%12s\n", farenheit_column, celsius_column);
+	while (farenheit <= upper){
+		celsius = 5 * (farenheit - 32) / 9;
+		printf("%12d%8d\n", farenheit, celsius);
+		farenheit = farenheit + step;
+	}
+	return 0;
+}
