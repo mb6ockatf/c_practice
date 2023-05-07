c38b2f5cae5eade7f408a41d905349a9933baee1
mb6ockatf <mb6ockatf@relay.firefox.com>
2023-05-07 14:23:51 +0300

    feat: add line_counter

---
+++ line_counter.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+	int character, lines;
+	lines = 0;
+	while ((character = getchar()) != EOF)
+		if (character == '\n')
+			++lines;
+	printf("%d\n", lines);
+	return 0;
+}
